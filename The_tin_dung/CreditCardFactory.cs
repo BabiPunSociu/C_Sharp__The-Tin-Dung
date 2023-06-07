ae3b7fda75fcc011e2df291715d318c35a1bf1d0
BabiPunSociu <Nguyendung281002@gmail.com>
2023-06-07 15:58:52 +0700

    DungNguyen

---
+++ The_tin_dung/CreditCardFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*    Tạo class CreditCardFactory để triển khai Factory Design Pattern. 
+    Bên trong tạo một phương thức nhận vào loại thẻ tín dụng,
+    khi đó sẽ hiển thị các thông tin của thẻ tín dụng đó
+*/
+
+
+
+namespace The_tin_dung
+{
+    internal class CreditCardFactory
+    {
+        public static CreditCard GetCreditCard(string cardType)
+        {
+            CreditCard cardDetail = null;
+            if (cardType == "MoneyBack")
+                cardDetail = new MoneyBack();
+            else if (cardType == "Titanium")
+                cardDetail = new Titanium();
+            else if (cardType == "Platinum")
+                cardDetail = new Platinum();
+            return cardDetail;
+        }
+    }
+}
---
+++ The_tin_dung/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace The_tin_dung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhập CardType: ");
+            string Card_Type = Console.ReadLine();
+
+            CreditCard creditDetails = CreditCardFactory.GetCreditCard(Card_Type);
+
+            if(creditDetails == null)
+                Console.WriteLine("Invalid Card Type");
+            else
+            {
+                Console.WriteLine("CardType: " + creditDetails.GetCardType());
+                Console.WriteLine("CreditLimit: " + creditDetails.GetCreditLimit());
+                Console.WriteLine("AnnualCharge: " + creditDetails.GetAnnualCharge());
+            }    
+        }
+    }
+}
